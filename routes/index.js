2159e5b8baba7c367be1765564dc0b956f52a369
Adam Ewell <adamantt147@aol.com>
2026-01-15 02:01:48 -0500

    Add 404 handler for non-existent routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const router = require("express").Router();
 const usersRouter = require("./users");
 const clothingItemsRouter = require("./clothingitem");
+const httpStatusCodes = require("../utils/errors");
 
 router.use("/items", clothingItemsRouter);
 
 router.use("/users", usersRouter);
+
+// 404 handler for non-existent routes
+router.use((req, res) => {
+  res
+    .status(httpStatusCodes.NOT_FOUND)
+    .json({ message: "Requested resource not found" });
+});
 
 module.exports = router;
